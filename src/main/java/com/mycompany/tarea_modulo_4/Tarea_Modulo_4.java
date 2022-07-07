dcd3121e8d3c1e8d1f3c7f81e23a73e7b55d2b24
OscarMauricio88 <108775435+OscarMauricio88@users.noreply.github.com>
2022-07-07 15:48:22 -0600

    Creando rama

---
+++ src/main/java/com/mycompany/tarea_modulo_4/Tarea_Modulo_4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tarea_modulo_4;
+
+/**
+ *
+ * @author Gladys Sosa
+ */
+public class Tarea_Modulo_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Tarea Modulo 4");
+    }
+}
